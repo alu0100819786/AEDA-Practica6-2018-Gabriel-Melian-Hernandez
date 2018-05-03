795377fd89f220d32a93f7326e13e0321f21f679
Gabriel Melian Hernandez <alu0100819786@ull.edu.es>
2018-05-03 11:46:22 +0100

    Add files via upload

---
+++ arbol.hpp
@@ -0,0 +1,298 @@
+#include "nodo.hpp"
+
+
+#pragma once
+using namespace std;
+
+template<class tipo_dato>
+class arbol{
+private:
+	nodo<tipo_dato>* raiz_;
+	unsigned cp_esta;
+public:
+	arbol(void);
+	arbol(nodo<tipo_dato>* nodo_raiz);
+	~arbol(void);
+	void podar(nodo<tipo_dato>*& nodo_);
+
+	nodo<tipo_dato>* get_raiz(void);
+	bool empty(void) const;
+	bool empty(nodo<tipo_dato>* nodo) const;
+	unsigned size(void) const;
+	unsigned size(nodo<tipo_dato>* nodo) const;
+	unsigned height(void) const;
+	unsigned height(nodo<tipo_dato>* nodo) const;
+	bool leaf(nodo<tipo_dato>* nodo);
+
+	unsigned insertar(tipo_dato elemento);
+	unsigned insertar(nodo<tipo_dato>* nodo_, nodo<tipo_dato>*& raiz, unsigned ct_cp);
+	void eliminar(tipo_dato elemento);
+	void eliminar(tipo_dato elemento, nodo<tipo_dato>*& raiz);
+	void sustituir(nodo<tipo_dato>*& viejo, nodo<tipo_dato>*& cambio);
+	nodo<tipo_dato>* buscar(tipo_dato elemento);
+	nodo<tipo_dato>* buscar(nodo<tipo_dato>* nodo, tipo_dato elemento);
+
+	void imprimir_dato(nodo<tipo_dato>* nodo);
+	void pre_orden(void);
+	void post_orden(void);
+	void in_orden(void);
+	void nivel_orden(void);
+	void pre_orden(nodo<tipo_dato>* raiz);
+	void post_orden(nodo<tipo_dato>* raiz);
+	void in_orden(nodo<tipo_dato>* raiz);
+	void nivel_orden(nodo<tipo_dato>* raiz);
+
+	unsigned get_esta(void);
+};
+
+template<class tipo_dato>
+arbol<tipo_dato>::arbol (void):
+raiz_(nullptr),
+cp_esta(0)
+{}
+
+template<class tipo_dato>
+arbol<tipo_dato>::arbol (nodo<tipo_dato>* nodo_raiz):
+raiz_(nodo_raiz),
+cp_esta(0)
+{}
+
+template<class tipo_dato>
+arbol<tipo_dato>::~arbol(void){
+	podar(raiz_);
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::podar(nodo<tipo_dato>*& nodo_) {
+
+	if (empty(nodo_)) 
+		return;
+	podar(nodo_->izquierda());
+	podar(nodo_->derecha());
+
+	nodo_ = nullptr;
+}
+
+template<class tipo_dato>
+nodo<tipo_dato>* arbol<tipo_dato>::get_raiz(void){
+	return raiz_;
+}
+
+template<class tipo_dato>
+bool arbol<tipo_dato>::empty(void) const{
+    return empty(raiz_);
+}
+
+template<class tipo_dato>
+bool arbol<tipo_dato>::empty(nodo<tipo_dato>* nodo) const{
+    return nodo == nullptr;
+}
+
+template<class tipo_dato>
+unsigned arbol<tipo_dato>::size(void) const{
+    return size(raiz_);
+}
+
+template<class tipo_dato>
+unsigned arbol<tipo_dato>::size(nodo<tipo_dato>* nodo) const{
+    if(empty(nodo))
+        return 0;
+    return(1 + size(nodo->izquierda()) + size(nodo->derecha()));
+}
+
+template<class tipo_dato>
+unsigned arbol<tipo_dato>::height(void) const
+{
+    return height(raiz_);
+}
+
+template<class tipo_dato>
+unsigned arbol<tipo_dato>::height(nodo<tipo_dato>* nodo) const
+{
+    if(nodo == nullptr)
+        return 0;
+
+    return max(height(nodo->izquierda()), height(nodo->derecha()))+1;
+}
+
+template<class tipo_dato>
+bool arbol<tipo_dato>::leaf(nodo<tipo_dato>* nodo){
+	return !nodo->derecha() && !nodo->izquierda();
+}
+
+template<class tipo_dato>
+unsigned arbol<tipo_dato>::insertar(tipo_dato elemento){
+    nodo<tipo_dato>* nodo_ = new nodo<tipo_dato>(elemento);
+    return insertar(nodo_, raiz_, 0);
+}
+
+template<class tipo_dato>
+unsigned arbol<tipo_dato>::insertar(nodo<tipo_dato>* nodo_, nodo<tipo_dato>*& raiz, unsigned ct_cp){
+    ct_cp++;
+	if(raiz == nullptr){
+		raiz = nodo_;
+		return ct_cp;
+	}
+	if(nodo_->dato() <= raiz->dato())
+		ct_cp = insertar(nodo_, raiz->izquierda(), ct_cp);
+	else
+		ct_cp = insertar(nodo_, raiz->derecha(), ct_cp);
+	
+	return ct_cp;
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::eliminar(tipo_dato elemento){
+    eliminar(elemento, raiz_);
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::eliminar(tipo_dato elemento, nodo<tipo_dato>*& raiz){
+    if(raiz == nullptr)
+        return;
+
+    if(elemento < raiz->dato())
+        eliminar(elemento, raiz->izquierda());
+    else if(elemento > raiz->dato())
+        eliminar(elemento, raiz->derecha());
+    else{
+        nodo<tipo_dato>* viejo = raiz;
+
+        if(raiz->derecha() == nullptr)
+            raiz = raiz->izquierda();
+        else if(raiz->izquierda() == nullptr)
+            raiz = raiz->derecha();
+        else
+            sustituir(viejo, raiz->izquierda());
+
+        delete viejo;
+    }
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::sustituir(nodo<tipo_dato>*& viejo, nodo<tipo_dato>*& cambio){
+    if(cambio->derecha() != nullptr)
+        sustituir(viejo, cambio->derecha());
+    else{
+        viejo->dato() = cambio->dato();
+        viejo = cambio;
+        cambio = cambio->izquierda();
+    }
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::imprimir_dato(nodo<tipo_dato>* nodo){
+    if(nodo == nullptr){
+        cout << "[.]";
+        return;
+    }
+	cout << "[ " << nodo->dato() << " ]" << " ";
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::pre_orden(void){
+    pre_orden(raiz_);
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::post_orden(void){
+    post_orden(raiz_);
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::in_orden(void){
+    in_orden(raiz_);
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::nivel_orden(void){
+	nivel_orden(raiz_);
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::pre_orden(nodo<tipo_dato>* raiz){
+    if(raiz == nullptr)
+        return;
+    imprimir_dato(raiz);
+    pre_orden(raiz->izquierda());
+    pre_orden(raiz->derecha());
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::post_orden(nodo<tipo_dato>* raiz){
+    if(raiz == nullptr)
+        return;
+    post_orden(raiz->izquierda());
+    post_orden(raiz->derecha());
+    imprimir_dato(raiz);
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::in_orden(nodo<tipo_dato>* raiz){
+    if(raiz == nullptr)
+        return;
+    in_orden(raiz->izquierda());
+    imprimir_dato(raiz);
+    in_orden(raiz->derecha());
+}
+
+template<class tipo_dato>
+void arbol<tipo_dato>::nivel_orden(nodo<tipo_dato>* raiz){
+	deque<pair<nodo<tipo_dato>*, unsigned>> cola;
+	nodo<tipo_dato>* copia_nodo;
+	unsigned nivel = 0, nivel_actual = 0;
+
+	cola.push_back(pair<nodo<tipo_dato>*, unsigned>(raiz,0));
+
+	cout << "Nivel 0: ";
+	while(!cola.empty()){
+		copia_nodo = std::get<0>(cola.front());
+        nivel = std::get<1>(cola.front());
+		cola.pop_front();
+		if(nivel > nivel_actual){
+			nivel_actual = nivel;
+			cout << endl << "Nivel " << nivel_actual << ": ";
+		}
+
+		if (copia_nodo != nullptr){
+			imprimir_dato(copia_nodo);
+			cola.push_back(pair<nodo<tipo_dato>*, unsigned>(copia_nodo -> izquierda(), nivel+1));
+			cola.push_back(pair<nodo<tipo_dato>*, unsigned>(copia_nodo -> derecha(), nivel+1));
+		}
+		else{
+			imprimir_dato(nullptr);
+		}
+
+	}
+}
+
+template<class tipo_dato>
+nodo<tipo_dato>* arbol<tipo_dato>::buscar(tipo_dato elemento){
+	cp_esta = 0; 
+	return buscar(raiz_, elemento);
+}
+
+template<class tipo_dato>
+nodo<tipo_dato>* arbol<tipo_dato>::buscar(nodo<tipo_dato>* nodo, tipo_dato elemento){
+
+	if (nodo == nullptr){
+		cp_esta ++;
+		return nullptr;
+	}
+
+	if (elemento == nodo->dato()){
+		cp_esta ++;
+		return nodo;
+	}
+
+	if (elemento < nodo->dato()){
+		cp_esta ++;
+		return buscar(nodo->izquierda(), elemento);
+	}
+	return buscar(nodo->derecha(), elemento);
+}
+
+template<class tipo_dato>
+unsigned arbol<tipo_dato>::get_esta(void){
+	return cp_esta;
+}
---
+++ main.cpp
@@ -0,0 +1,177 @@
+
+#include "common.hpp"
+#include "DNI.hpp"
+#include "insercion.hpp"
+#include "nodo.hpp"
+#include "arbol.hpp"
+
+
+
+using namespace std;
+
+unsigned opc_demo =4;
+
+
+void error(void){
+	cin.ignore();
+	cout << endl << "La opcion introducida no es valida. Presione enter e introduzca otra.";
+	cin.ignore();
+	
+}
+
+void calcular_cp(vector<float> &com, vector<vector<float>> &v, float sm, unsigned int modo_){
+	
+	insercion<float>(com,1);
+
+	v[modo_][0] = com.front();
+	v[modo_][1] = ((com.front()+com.back())/2);
+	v[modo_][2] = com.back();
+}
+
+int main (void){
+	
+	srand(time(NULL));
+
+	unsigned opc = 3;
+	int ct_pruebas = 300;
+	int tam_arbol, num_rep;
+
+	while (opc != 0 && opc != 1 && opc != 2){
+		cout << "================ " << "ABB" << " =================" << endl;
+		cout << "Seleccione el modo con el que desea trabajar: " << endl;
+		cout << "0. Modo demostración" << endl;
+		cout << "1. Modo estadisticas" << endl;
+		cout << "2. Abandonar" << endl;
+		cout << "Introduzca una opcion: ";
+		cin >> opc;
+		if (opc != 0 && opc != 1 && opc != 2)
+			error();
+	}
+
+	if (opc==0){
+		arbol<int> arbol_demo;
+		do{
+		while (opc_demo != 0 && opc_demo != 1 && opc_demo != 2 && opc_demo != 3){ 
+			write(1,"\033[H\033[2J",7);
+			cout << "============= " << "MODO DEMOSTRACIÓN" << " =============" << endl;
+			cout << "0. Insertar Clave" << endl;
+			cout << "1. Eliminar Clave" << endl;
+			cout << "2. Mostrar Árbol" << endl;
+			cout << "3. Abandonar" << endl;
+			cout << "Introduzca una opcion: ";
+			cin >> opc_demo;
+			if (opc_demo != 0 && opc_demo != 1 && opc_demo != 2 && opc_demo != 3)
+				error();
+		}
+		
+		
+		switch (opc_demo){
+			case 0:{
+			
+				int mat;
+				
+				cout << "============= " << "MODO DEMOSTRACIÓN CON INT" << " =============" << endl;
+				cout << "Introduzca el número a insertar en el árbol: ";
+				cin >> mat;
+				arbol_demo.insertar(mat);
+				cout << "El numero insertado es " << mat << endl;
+				cin.ignore(); cin.ignore();
+				opc_demo = 4;
+				break;
+			}
+			
+			case 1:{
+				
+				int mat;
+			
+				cout << "============= " << "MODO DEMOSTRACIÓN CON INT" << " =============" << endl;
+				cout << "Introduzca el número a eliminar en el árbol: ";
+				cin >> mat;
+				arbol_demo.eliminar(mat);
+				cout << "El numero eliminado es " << mat << endl;
+				cin.ignore(); cin.ignore();
+				opc_demo = 4;
+				break;
+			}
+			case 2:{
+					
+				
+				cout << "============= " << "MODO DEMOSTRACIÓN CON INT" << " =============" << endl;
+				arbol_demo.nivel_orden();
+				cin.ignore(); cin.ignore();
+				opc_demo = 4;
+				break;
+			}
+			case 3:{
+				cout << "=============" << "SALIENDO" << "=============" << endl;
+				return 0;
+				break;
+			}
+
+		}
+		}while(opc_demo != 3);
+	}
+	else if (opc==1){
+		
+		cout << "============= " << "MODO ESTADÍSTICA" << " =============" << endl;
+		cout << "Introduzca el tamaño del arbol a analizar: ";
+		cin >> tam_arbol;
+		cout << "Introduzca la cantidad de veces que quiera que se repitan las pruebas: ";
+		cin >> num_rep;
+		cout << endl << "                               NUMERO DE COMPARACIONES" << endl;
+		cout << "                            Minimo -  Promedio  -   Maximo" << endl;
+
+		vector<DNI> vector_DNI(tam_arbol);
+		vector<vector<float>> resultados(2, vector<float>(3));
+		vector<float> comp(vector_DNI.size());
+		float suma = 0, suma_intermedia =0;
+
+			for (int j=0; j<num_rep; j++){
+			arbol<DNI> arbol_esta;
+			for (int k=0;k<vector_DNI.size();k++)
+				vector_DNI[k].set_random();
+			for (int k=0;k<vector_DNI.size();k++){
+				comp[k] = arbol_esta.insertar(vector_DNI[k]);
+			}
+		}
+
+					calcular_cp(comp, resultados, suma, 0);
+
+					cout << "Comparaciones insercion: " << setw(12) << resultados[0][0] << setw(12) << resultados[0][1] << setw(12) << resultados[0][2] << endl;
+					suma=0;
+					
+					vector<DNI> vector_DNI_2(tam_arbol*2);
+					vector<float> comp_2(vector_DNI_2.size());
+
+		for (int j=0; j<num_rep; j++){
+			for (int k=0;k<vector_DNI_2.size();k++)
+				vector_DNI_2[k].set_random();
+				arbol<DNI> arbol_esta;
+			for (int k=0;k<vector_DNI_2.size()/2;k++){
+				arbol_esta.insertar(vector_DNI_2[k]);
+			}
+			for (int k=0;k<vector_DNI_2.size()/2;k++){
+				 arbol_esta.buscar(vector_DNI_2[k]);
+				 comp_2[k] = arbol_esta.get_esta();
+			}
+			for (int k=vector_DNI_2.size()/2; k<vector_DNI_2.size(); k++){
+				arbol_esta.buscar(vector_DNI_2[k]);
+				comp_2[k] += arbol_esta.get_esta();
+			}
+		}
+
+		calcular_cp(comp_2, resultados, suma, 0);
+
+		cout << "Comparaciones búsqueda:  " << setw(12) << resultados[0][0] << setw(12) << resultados[0][1] << setw(12) << resultados[0][2] << endl;
+		suma=0;
+
+	}
+else{
+	cout << endl << "Abandonando.." << endl;
+	return 0;
+}
+
+return 0;
+
+				
+}
